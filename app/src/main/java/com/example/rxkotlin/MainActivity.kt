a21b1f6a29c39b530ee5b6a9b05260c399745a69
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2021-02-01 15:42:10 +0500

    Initial commit

---
+++ app/src/main/java/com/example/rxkotlin/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.rxkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import io.reactivex.rxjava3.subjects.PublishSubject
+import io.reactivex.rxjava3.subjects.Subject
+import java.util.*
+
+private const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        lambdaExpressionExample()
+        inlineFunctionsExample()
+        reactiveEvenOdd()
+    }
+
+
+    private fun lambdaExpressionExample()
+    {
+        val sum = { x : Int, y : Int -> x + y }
+        val anonyMousMult = { x : Int -> (Random().nextInt(15) + 1) * x }
+
+        Log.d(TAG, "functionalProgrammingExample: ############################ Print Sum : " + sum(12, 14))
+        Log.d(TAG, "functionalProgrammingExample: ############################ Print Mult : " + anonyMousMult(2) )
+    }
+
+    private fun inlineFunctionsExample()
+    {
+        fun doSomething(a:Int = 0) = a + (a * a);
+        for (i in 1..5)
+        {
+            Log.d(TAG, "inlineFunctionsExample: ############################ doSomething() : " + doSomething(i) );
+        }
+
+    }
+
+    private fun reactiveEvenOdd()
+    {
+        val subject: Subject<Int> = PublishSubject.create();
+
+        subject.map({ isEven(it) }).subscribe({ Log.d(TAG, "reactiveEvenOdd: ############################ " + "The Number is ${(if(it) "Even" else "Odd")}  " ) });
+        subject.onNext(4);
+        subject.onNext(7);
+    }
+
+    private fun isEven(x: Int): Boolean {
+        return x % 2 == 0
+    }
+}
